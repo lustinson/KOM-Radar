5172e087d2f5dd472fbf8502042f6d2de235daca
Lukas Stinson <stinser01@gmail.com>
2026-05-17 13:47:59 -0700

    warning error fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import "@/app/globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://komradar.com"),
